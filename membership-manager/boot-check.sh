89fd70899132e85500dc9e989330adf8c4b61508
Lucas Ferreira da Silva <lferreira@inf.ufsm.br>
2021-08-28 10:32:18 -0300

    Clean DocWorker Derfile and fix boot-ckeck script bugs

--- membership-manager/boot-check.sh
+++ membership-manager/boot-check.sh
@@ -1,22 +1,47 @@
 #!/bin/sh
 
-set -e
+function wait_for_coordinator()
+{
+    local server=$1
+    local port=$2
+
+    let i=1
+
+    nc -z $server $port
+    nc_result=$?
+
+    until [ $nc_result -eq 0 ]; do
+      echo "Trying connect to coordinator: ${server}:${port}..."
+      echo "Coordinator ${server}:${port} is not available yet"
+      let "i++"
+      sleep 2
+
+      nc -z $server $port
+      nc_result=$?
+    done
+
+    echo "Coordinator ${server}:${port} is available!"
+}
 
 COORDINATOR_SERVICE=${CITUS_COORDINATOR:=coordinator}
-COORDINATOR_PORT=${CITUS_COORDINATOR:=5432}
+COORDINATOR_PORT=${COORDINATOR_PORT:=5432}
 PG_USER=${POSTGRES_USER:=postgres}
 
 echo "CITUS_COORDINATOR service set as: $COORDINATOR_SERVICE"
 echo "POSTGRES_USER set as: $PG_USER"
 
-# make sure coordinator is ready to accept connections
-COORDINATOR_IP=$(getent hosts tasks.$COORDINATOR_SERVICE)
-while ! nc -z $COORDINATOR_IP $COORDINATOR_PORT
-do
-  echo "Waiting for Citus coordinator from service $COORDINATOR_SERVICE"
-  sleep 2
-done
+# wait until coordinator service ip is reachable
+until [[ "$(getent hosts tasks.$COORDINATOR_SERVICE)" ]]
+do 
+    echo "Waiting for Coordinator node boot"
+    sleep 2
+done 
 
-echo "Citus coordinator node is ready in: $COORDINATOR_IP"
+COORDINATOR_IP=$(getent hosts tasks.$COORDINATOR_SERVICE | awk '{ print $1}')
+echo "Citus coordinator node ip: $COORDINATOR_IP"
+
+# make sure coordinator is ready to accept connections
+wait_for_coordinator "$COORDINATOR_IP" "$COORDINATOR_PORT"
+
 echo "Starting membership manager..."
 
